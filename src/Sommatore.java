1471f44bd1ac62e5c526d6c80984b9bb7d59be9d
Radu Grigoras <grigorasradu40@gmail.com>
2018-02-19 12:41:23 +0100

    aggiunto somma200

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -28,6 +28,14 @@ public class Sommatore
 		
 		
 	}
+	public static int somma200(int x)
+	{
+		for (int i = 0; i < 200; i++) 
+			x++;
+		return x;
+		
+		
+	}
 	public static void main(String[] args) 
 	{
 		int x=1;
@@ -35,7 +43,7 @@ public class Sommatore
 		System.out.println(somma10(x));
 		System.out.println(somma20(x));
 		System.out.println(somma100(x));
-
+		System.out.println(somma200(x));
 	}
 
 }
